051d6b84939e14332af3c14893b4dde2091f0010
Azrael <584917180@qq.com>
2017-05-01 15:23:12 +0800

    添加新方法findByType

--- src/cn/azrael/main/facilitie/service/FacilitieService.java
+++ src/cn/azrael/main/facilitie/service/FacilitieService.java
@@ -1,8 +1,10 @@
 package cn.azrael.main.facilitie.service;
 
+import java.util.List;
+
 import cn.azrael.main.core.service.BaseService;
 import cn.azrael.main.facilitie.entity.Facilitie;
 
 public interface FacilitieService extends BaseService<Facilitie>{
-
+	public List<Facilitie> findByType(int type);
 }
--- src/cn/azrael/main/facilitie/service/impl/FacilitieServiceImpl.java
+++ src/cn/azrael/main/facilitie/service/impl/FacilitieServiceImpl.java
@@ -1,10 +1,13 @@
 package cn.azrael.main.facilitie.service.impl;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
 import cn.azrael.main.core.service.impl.BaseServiceImpl;
+import cn.azrael.main.core.util.QueryHelper;
 import cn.azrael.main.facilitie.dao.FacilitieDao;
 import cn.azrael.main.facilitie.entity.Facilitie;
 import cn.azrael.main.facilitie.service.FacilitieService;
@@ -15,5 +18,11 @@ public class FacilitieServiceImpl extends BaseServiceImpl<Facilitie> implements 
 	public void setFacilitieDao(FacilitieDao facilitieDao) {
 		setBaseDao(facilitieDao);
 		this.facilitieDao = facilitieDao;
+	}
+	@Override
+	public List<Facilitie> findByType(int type) {
+		QueryHelper queryHelper = new QueryHelper(Facilitie.class, "f");
+		queryHelper.addCondition("f.type=?", type);
+		return facilitieDao.findObjects(queryHelper);
 	}
 }
