6ee5118ff8381f34909f073a70553b4adfe42ad1
Azrael <584917180@qq.com>
2017-05-07 14:13:48 +0800

    修改faacilitiefix的status字段后修改测试方法

--- src/cn/azrael/test/TestFacilitie.java
+++ src/cn/azrael/test/TestFacilitie.java
@@ -1,21 +1,30 @@
 package cn.azrael.test;
 
 
+import java.util.Date;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import cn.azrael.main.facilitie.entity.Facilitie;
+import cn.azrael.main.facilitie.entity.FacilitieFix;
+import cn.azrael.main.facilitie.service.FacilitieFixService;
 import cn.azrael.main.facilitie.service.FacilitieService;
+import cn.azrael.main.user.service.EmployeeService;
 
 public class TestFacilitie {
 	private ApplicationContext ac;
 	private FacilitieService fs;
+	private FacilitieFixService ffs;
+	private EmployeeService es;
 	@Before
 	public void loadCtx(){
 		ac = new ClassPathXmlApplicationContext(new String[] {"classpath:spring.xml"});
 		fs = (FacilitieService) ac.getBean("facilitieService");
+		ffs = (FacilitieFixService) ac.getBean("facilitieFixService");
+		es = (EmployeeService) ac.getBean("employeeService");
 	}
 	
 	@Test
@@ -27,6 +36,18 @@ public class TestFacilitie {
 			f.setNormalNum(100+i);
 			f.setBadNum(i);
 			fs.save(f);
+		}
+	}
+	@Test
+	public void testFacilitieFixSave() throws Exception {
+		FacilitieFix ff = new FacilitieFix();
+		ff.setDateBegin(new Date().getTime()/1000.0);
+		for(int i = 0;i < 10;i++){
+			ff.setEmployeeId(es.findObjects().get(i));
+			ff.setStatus(1);
+			ff.setContnet("马桶坏了");
+			ff.setFacilitieId(fs.findObjects().get(i));
+			ffs.save(ff);
 		}
 	}
 }
