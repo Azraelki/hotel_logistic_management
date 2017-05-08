c4804af05058c1e4581899f83a7d76d48985474e
Azrael <584917180@qq.com>
2017-05-08 17:23:39 +0800

    添加通过员工ID查找user

--- src/cn/azrael/main/user/dao/UserDao.java
+++ src/cn/azrael/main/user/dao/UserDao.java
@@ -4,5 +4,6 @@ import cn.azrael.main.core.dao.BaseDao;
 import cn.azrael.main.user.entity.User;
 
 public interface UserDao extends BaseDao<User>{
-
+	//通过员工id查找user
+	public User findByEmployeeId(String id);
 }
--- src/cn/azrael/main/user/dao/impl/UserDaoImpl.java
+++ src/cn/azrael/main/user/dao/impl/UserDaoImpl.java
@@ -1,9 +1,23 @@
 package cn.azrael.main.user.dao.impl;
 
+import java.util.List;
+
 import cn.azrael.main.core.dao.impl.BaseDaoImpl;
+import cn.azrael.main.core.util.QueryHelper;
 import cn.azrael.main.user.dao.UserDao;
 import cn.azrael.main.user.entity.User;
 
 public class UserDaoImpl extends BaseDaoImpl<User> implements UserDao{
+
+	@Override
+	public User findByEmployeeId(String id) {
+		QueryHelper queryHelper = new QueryHelper(User.class, "u");
+		queryHelper.addCondition("u.employeeId.id=?", id);
+		List<User> list = findObjects(queryHelper);
+		if(list!=null&&list.size()>0){
+			return list.get(0);
+		}
+		return null;
+	}
 
 }
