4c6737c9feb0469892742857b063ef007bf190aa
Azrael <584917180@qq.com>
2017-05-08 17:29:17 +0800

    更新login模块的action及配置文件

--- src/cn/azrael/main/login/action/LoginAction.java
+++ src/cn/azrael/main/login/action/LoginAction.java
@@ -1,9 +1,73 @@
 package cn.azrael.main.login.action;
 
+import javax.annotation.Resource;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.struts2.ServletActionContext;
+
+import cn.azrael.main.core.constant.Constant;
+import cn.azrael.main.user.entity.User;
+import cn.azrael.main.user.service.UserService;
+
 import com.opensymphony.xwork2.ActionSupport;
 
 public class LoginAction extends ActionSupport{
-	public String login(){
-		return SUCCESS;
+	@Resource
+	private UserService userService;
+	private User user;
+	private String loginResult;
+	//跳转到登录界面
+	public String toLoginUI(){
+		return "loginUI";
 	}
+	//登陆
+	public String login(){
+		if(user!=null && user.getEmployeeId()!=null){
+			if(StringUtils.isNotBlank(user.getEmployeeId().getPhoneNumber())&&StringUtils.isNotBlank(user.getPassword())){
+				//根据电话和密码查询用户
+				User u = userService.findByPhoneAndPassword(user.getEmployeeId().getPhoneNumber(),user.getPassword());
+				if(u!=null){
+					//登陆成功
+					ServletActionContext.getRequest().getSession().setAttribute(Constant.USER, u);
+					//将用户登录信息保存到日志中
+					Log log = LogFactory.getLog(getClass());
+					log.info("用户为："+ u.getEmployeeId().getName()+" 的用户登陆了系统。");
+					//重定向到首页
+					return "home";
+				}else{
+					loginResult = "帐号或密码不正确！";
+				}
+			}else{
+				loginResult = "帐号或密码不能为空！";
+			}
+		}else{
+			loginResult = "请输入帐号和密码！";
+		}
+		return toLoginUI();
+	}
+	//退出，注销
+	public String logout(){
+		//清除session中的用户信息
+		ServletActionContext.getRequest().getSession().removeAttribute(Constant.USER);
+		return toLoginUI();
+	}
+	//跳转到没有权限提示页面
+	public String toNoPermissionUI(){
+		return "noPermissionUI";
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public String getLoginResult() {
+		return loginResult;
+	}
+	public void setLoginResult(String loginResult) {
+		this.loginResult = loginResult;
+	}
+	
 }
