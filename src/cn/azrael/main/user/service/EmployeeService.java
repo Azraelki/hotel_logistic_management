07f32918fca938159eab4f6ac1423f4b0138dbb1
Azrael <584917180@qq.com>
2017-05-08 17:27:15 +0800

    employeeService添加以下三个方法  --》添加员工是判断其职务增添相应的role  --》删除员工并删除其登陆功能  --》更新并判断其是否有必要更改登陆的权限

--- src/cn/azrael/main/user/service/EmployeeService.java
+++ src/cn/azrael/main/user/service/EmployeeService.java
@@ -10,4 +10,10 @@ public interface EmployeeService extends BaseService<Employee>{
 	 * @return 员工实体
 	 */
 	public Employee findByName(String name);
+	//添加员工是判断其职务增添相应的role
+	public void addEmployeeAndSaveUser(Employee employee);
+	//删除员工并删除其登陆功能
+	public void deleteAndDeleteUser(String id);
+	//更新并判断其是否有必要更改登陆的权限
+	public void updateAndUpdateUser(Employee employee);
 }
--- src/cn/azrael/main/user/service/impl/EmployeeServiceImpl.java
+++ src/cn/azrael/main/user/service/impl/EmployeeServiceImpl.java
@@ -9,11 +9,18 @@ import org.springframework.stereotype.Service;
 import cn.azrael.main.core.service.impl.BaseServiceImpl;
 import cn.azrael.main.core.util.QueryHelper;
 import cn.azrael.main.user.dao.EmployeeDao;
+import cn.azrael.main.user.dao.UserDao;
 import cn.azrael.main.user.entity.Employee;
+import cn.azrael.main.user.entity.User;
 import cn.azrael.main.user.service.EmployeeService;
 @Service("employeeService")
 public class EmployeeServiceImpl extends BaseServiceImpl<Employee> implements EmployeeService{
 	private EmployeeDao employeeDao;
+	private UserDao userDao;
+	@Resource
+	public void setUserDao(UserDao userDao) {
+		this.userDao = userDao;
+	}
 	@Resource
 	public void setEmployeeDao(EmployeeDao employeeDao) {
 		this.setBaseDao(employeeDao);
@@ -28,5 +35,53 @@ public class EmployeeServiceImpl extends BaseServiceImpl<Employee> implements Em
 			return el.get(0);
 		else
 			return null;
+	}
+	@Override
+	public void addEmployeeAndSaveUser(Employee employee) {
+		int num = employee.getJobId().getId();
+		employeeDao.save(employee);
+		if(num == 1 || num == 2 || num == 5 || num == 6){
+			employee = findByName(employee.getName());
+			User u = new User();
+			u.setEmployeeId(employee);
+			u.setType(num);
+			u.setPassword("123456");
+			userDao.save(u);
+		}
+		
+	}
+	@Override
+	public void deleteAndDeleteUser(String id) {
+		Employee employee = employeeDao.findObjectById(id);
+		int num = employee.getJobId().getId();
+		if(num == 1 || num == 2 || num == 5 || num == 6){
+			User user = userDao.findByEmployeeId(employee.getId());
+			if(user!=null)
+				userDao.delete(user.getId());
+		}
+		employeeDao.delete(employee.getId());
+	}
+	@Override
+	public void updateAndUpdateUser(Employee employee) {
+		int num = employee.getJobId().getId();
+		employeeDao.update(employee);
+		User u = userDao.findByEmployeeId(employee.getId());
+		if(u!=null){
+			if(num!=u.getType() && (num == 1 || num == 2 || num == 5 || num == 6)){
+				u.setType(num);
+				userDao.update(u);
+			}else{
+				userDao.delete(u.getId());
+			}
+		}else{
+			if(num == 1 || num == 2 || num == 5 || num == 6){
+				u = new User();
+				u.setEmployeeId(employee);
+				u.setPassword("123456");
+				u.setType(num);
+				userDao.save(u);
+			}
+		}
+		
 	}
 }
