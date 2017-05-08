88ae872ab7ea9ec0953a28df0881fa027899143f
Azrael <584917180@qq.com>
2017-05-08 17:28:09 +0800

    添加用户service及实现

---
+++ src/cn/azrael/main/user/service/UserService.java
@@ -0,0 +1,14 @@
+package cn.azrael.main.user.service;
+
+import java.util.List;
+
+import cn.azrael.main.core.service.BaseService;
+import cn.azrael.main.user.entity.Role;
+import cn.azrael.main.user.entity.User;
+
+public interface UserService extends BaseService<User>{
+	//根据用户返回权限集合
+	public List<Role> findRoleByUser(User user);
+	//根据员工电话和用户密码查询员工
+	public User findByPhoneAndPassword(String phone,String password);
+}
---
+++ src/cn/azrael/main/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,58 @@
+package cn.azrael.main.user.service.impl;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import cn.azrael.main.core.service.impl.BaseServiceImpl;
+import cn.azrael.main.core.util.QueryHelper;
+import cn.azrael.main.user.dao.EmployeeDao;
+import cn.azrael.main.user.dao.RoleDao;
+import cn.azrael.main.user.dao.UserDao;
+import cn.azrael.main.user.entity.Employee;
+import cn.azrael.main.user.entity.Role;
+import cn.azrael.main.user.entity.User;
+import cn.azrael.main.user.service.UserService;
+
+@Service("userService")
+public class UserServiceImpl extends BaseServiceImpl<User> implements UserService{
+	private UserDao userDao;
+	private RoleDao roleDao;
+	private EmployeeDao employeeDao;
+	@Resource
+	public void setEmployeeDao(EmployeeDao employeeDao) {
+		this.employeeDao = employeeDao;
+	}
+	@Resource
+	public void setRoleDao(RoleDao roleDao) {
+		this.roleDao = roleDao;
+	}
+	@Resource
+	public void setUserDao(UserDao userDao) {
+		setBaseDao(userDao);
+		this.userDao = userDao;
+	}
+	@Override
+	public List<Role> findRoleByUser(User user) {
+		user = userDao.findObjectById(user.getId());
+		QueryHelper queryHelper = new QueryHelper(Role.class, "r");
+		queryHelper.addCondition("r.type=?", user.getType());
+		return roleDao.findObjects(queryHelper);
+	}
+	@Override
+	public User findByPhoneAndPassword(String phone, String password) {
+		Employee e = employeeDao.findByPhone(phone);
+		if(e == null)
+			return null;
+		QueryHelper queryHelper = new QueryHelper(User.class, "u");
+		queryHelper.addCondition("u.employeeId.id=?", e.getId());
+		queryHelper.addCondition("u.password=?", password);
+		List<User> list = userDao.findObjects(queryHelper);
+		if(list!=null && list.size()>0){
+			return list.get(0);
+		}else
+			return null;
+	}
+}
