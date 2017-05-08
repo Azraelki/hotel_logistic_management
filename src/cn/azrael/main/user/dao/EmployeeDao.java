28c5558fe780393b8f36bccac31bc65ce2fee13d
Azrael <584917180@qq.com>
2017-05-08 17:23:01 +0800

    添加通过电话号码获取员工方法

--- src/cn/azrael/main/user/dao/EmployeeDao.java
+++ src/cn/azrael/main/user/dao/EmployeeDao.java
@@ -4,5 +4,6 @@ import cn.azrael.main.core.dao.BaseDao;
 import cn.azrael.main.user.entity.Employee;
 
 public interface EmployeeDao extends BaseDao<Employee>{
-
+	//根据电话号码查询员工
+	public Employee findByPhone(String phoneNum);
 }
--- src/cn/azrael/main/user/dao/impl/EmployeeDaoImpl.java
+++ src/cn/azrael/main/user/dao/impl/EmployeeDaoImpl.java
@@ -1,9 +1,23 @@
 package cn.azrael.main.user.dao.impl;
 
+import java.util.List;
+
 import cn.azrael.main.core.dao.impl.BaseDaoImpl;
+import cn.azrael.main.core.util.QueryHelper;
 import cn.azrael.main.user.dao.EmployeeDao;
 import cn.azrael.main.user.entity.Employee;
 
 public class EmployeeDaoImpl extends BaseDaoImpl<Employee> implements EmployeeDao{
+
+	@Override
+	public Employee findByPhone(String phoneNum) {
+		QueryHelper queryHelper = new QueryHelper(Employee.class, "e");
+		queryHelper.addCondition("e.phoneNumber=?", phoneNum);
+		List<Employee> list = findObjects(queryHelper);
+		if(list!=null && list.size()>0){
+			return list.get(0);
+		}else
+			return null;
+	}
 
 }
